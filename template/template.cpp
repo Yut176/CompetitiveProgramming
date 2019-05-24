a60ad9b2b8ee5496196da540ec540b48d78bca53
Yut176 <yutsk176dev@gmail.com>
2019-05-24 12:17:51 +0900

    Add template

---
+++ template/template.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long int mod = 1e9+7;
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
+//////////////////////////////////
+
+int main(){
+
+  return 0;
+}
+
+// EOF
