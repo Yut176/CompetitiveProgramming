4bfd6890f0114775f7131806138156519f3b34c1
Yut176 <yutsk176dev@gmail.com>
2019-08-20 18:08:59 +0900

    Update template

--- template/template.cpp
+++ template/template.cpp
@@ -1,8 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
-long long int mod = 1e9+7;
-int dx[4] = {0,1,0,-1};
-int dy[4] = {1,0,-1,0};
+#define all(x) (x).begin(),(x).end()
+const long long mod = 1e9+7;
+const int dx[4] = {0,1,0,-1};
+const int dy[4] = {1,0,-1,0};
 //////////////////////////////////
 
 int main(){
